605b2ef9a427071afb9a62398cc058b901f11588
Jhoanck7 <jhoan.montero@alumnos.ucn.cl>
2025-11-05 06:40:12 -0300

    fix(publish button): the button is working  now

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -95,7 +95,7 @@ export default function OffersPage() {
 
         {logged && (
           <Link
-            href="/publish"
+            href="/offerer"
             className="h-10 shrink-0 inline-flex items-center justify-center rounded-xl px-4 font-semibold text-white
                        bg-[var(--primary)] hover:opacity-95 transition mt-1"
           >
