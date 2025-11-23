e1598cf6b8a400db3aa3c655140482c53e02cd4b
unknown <meylin.leonario@alumnos.ucn.cl>
2025-11-23 16:28:13 -0300

    fix(reviews): student, employer and report evaluation views fixed.

--- src23/lib/utils.ts
+++ src23/lib/utils.ts
@@ -2,7 +2,7 @@ import {
     OfferForAdmin, 
     PendingOffersForAdmin, 
     BuySellBasic 
-} from "@/models/responses/publication";
+} from "@/models/responses";
 
 function toOfferTypeForAdmin(o: PendingOffersForAdmin): OfferForAdmin["type"] {
     const typeValue = (o.type ?? 0); 
