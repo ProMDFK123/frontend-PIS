28a42d26ba18fdee5b87aeed1a4fef945898baa8
Jhoanck7 <jhoan.montero@alumnos.ucn.cl>
2025-12-16 03:23:16 -0300

    feat(SiteHeader): update logo redirection for offerers

--- src/components/shared/SiteHeader.tsx
+++ src/components/shared/SiteHeader.tsx
@@ -138,10 +138,11 @@ export default function SiteHeader() {
 
   const isAdmin = auth.role === "Admin";
   const isOfferer = auth.role === "Offerent";
+  
   const mainLinks = isAdmin ? adminNavLinks : isOfferer ? offererNavLinks : userLinks;
 
   // Lógica para determinar a dónde redirige el Logo
-  const logoHref = isAdmin ? "/admin/publications" : "/";
+  const logoHref = isAdmin ? "/admin/publications" : isOfferer ? "/offerer" :"/";
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-[var(--border)] bg-white/80 backdrop-blur-xl">
